5771b01f587ed3856fe99a7ade926de0a739bd56
Aleksander Jaworski <aleck156@gmail.com>
2022-04-16 11:16:18 +0200

    join recipes into one string

--- src/js/controller.js
+++ src/js/controller.js
@@ -98,8 +98,9 @@ const showRecipe = async function () {
         <div class="recipe__ingredients">
           <h2 class="heading--2">Recipe ingredients</h2>
           <ul class="recipe__ingredient-list">
-            ${recipe.ingredients.map(ing => {
-              return `
+            ${recipe.ingredients
+              .map(ing => {
+                return `
               <li class="recipe__ingredient">
               <svg class="recipe__icon">
                 <use href="src/img/icons.svg#icon-check"></use>
@@ -111,7 +112,8 @@ const showRecipe = async function () {
               </div>
               </li>
               `;
-            })}
+              })
+              .join('')}
           </ul>
         </div>
 
