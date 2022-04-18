06e64c730474efd0b24edcce5df66fdcc36bcfdb
Aleksander Jaworski <aleck156@gmail.com>
2022-04-18 08:08:24 +0200

    copy code responsible for fetching recipe data from API

--- src/js/model.js
+++ src/js/model.js
@@ -2,7 +2,28 @@ export const state = {
   recipe: {},
 };
 
-export const loadRecipe = async function () {
+export const loadRecipe = async function (id) {
   try {
+    const res = await fetch(
+      // `https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886`
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
+    );
+    const resJSON = await res.json();
+    console.log(resJSON);
+    if (!res.ok) {
+      throw new Error(`${resJSON.message}`);
+    }
+
+    let { recipe } = resJSON.data;
+    recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceURL: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
   } catch (error) {}
 };
