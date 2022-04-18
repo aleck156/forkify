d7b2a6c430978144b0750edce9e0b143d72ca9ab
Aleksander Jaworski <aleck156@gmail.com>
2022-04-18 08:46:16 +0200

    replace recipe with this.#data

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -9,11 +9,11 @@ class RecipeView {
   #generateMarkup() {
     return `
         <figure class="recipe__fig">
-          <img src="${recipe.image}" alt="${
-      recipe.title
+          <img src="${this.#data.image}" alt="${
+      this.#data.title
     }" class="recipe__img" />
           <h1 class="recipe__title">
-            <span>${recipe.title}</span>
+            <span>${this.#data.title}</span>
           </h1>
         </figure>
 
@@ -23,7 +23,7 @@ class RecipeView {
               <use href="${icons}#icon-clock"></use>
             </svg>
             <span class="recipe__info-data recipe__info-data--minutes">${
-              recipe.cookingTime
+              this.#data.cookingTime
             }</span>
             <span class="recipe__info-text">minutes</span>
           </div>
@@ -32,7 +32,7 @@ class RecipeView {
               <use href="${icons}#icon-users"></use>
             </svg>
             <span class="recipe__info-data recipe__info-data--people">${
-              recipe.servings
+              this.#data.servings
             }</span>
             <span class="recipe__info-text">servings</span>
 
@@ -65,7 +65,7 @@ class RecipeView {
         <div class="recipe__ingredients">
           <h2 class="heading--2">Recipe ingredients</h2>
           <ul class="recipe__ingredient-list">
-            ${recipe.ingredients
+            ${this.#data.ingredients
               .map(ing => {
                 return `
               <li class="recipe__ingredient">
@@ -89,13 +89,13 @@ class RecipeView {
           <p class="recipe__directions-text">
             This recipe was carefully designed and tested by
             <span class="recipe__publisher">${
-              recipe.publisher
+              this.#data.publisher
             }</span>. Please check out
             directions at their website.
           </p>
           <a
             class="btn--small recipe__btn"
-            href="${recipe.sourceURL}"
+            href="${this.#data.sourceURL}"
             target="_blank"
           >
             <span>Directions</span>
