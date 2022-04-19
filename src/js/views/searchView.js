ae5418379aca25ca0fddcab40ce0cf4e28ffe443
Aleksander Jaworski <aleck156@gmail.com>
2022-04-19 11:05:55 +0200

    add clearInput method, call it after addHandlerSearch calls handler

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -5,11 +5,16 @@ class SearchView {
     return this.#parentEl.querySelector('.search__field').value;
   }
 
+  clearInput() {
+    this.#parentEl.querySelector('.search__field').value = '';
+  }
+
   addHandlerSearch(handler) {
     this.#parentEl.addEventListener('submit', e => {
       e.preventDefault();
       console.log(e);
       handler();
+      this.clearInput();
     });
   }
 }
