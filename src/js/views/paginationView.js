a6740682ae196a7231f47229883f35eee3fd112d
Aleksander Jaworski <aleck156@gmail.com>
2022-04-22 08:21:14 +0200

    grab parent element of pagination

--- src/js/views/paginationView.js
+++ src/js/views/paginationView.js
@@ -1,6 +1,8 @@
 import View from './View';
 import icons from '../../img/icons.svg';
 
-class PaginationView extends View {}
+class PaginationView extends View {
+  _parentElement = document.querySelector('.pagination');
+}
 
 export default new PaginationView();
