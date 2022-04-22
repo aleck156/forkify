03d8d15ea31fa6a322a7657bd6acfa9fd54adb36
Aleksander Jaworski <aleck156@gmail.com>
2022-04-22 08:47:13 +0200

    define conditions for first and last apges

--- src/js/views/paginationView.js
+++ src/js/views/paginationView.js
@@ -11,16 +11,21 @@ class PaginationView extends View {
     console.log(numPages);
     console.log(this);
     // page 1, and theere are other pages
+    if (this._data.page === 1 && numPages > 1) {
+      return `page 1 and others`;
+    }
 
     // page 1, but no other pages
 
     // last page, no pages left
+    if (this._data.page === numPages) {
+      return `last and others`;
+    }
 
     // mid page
     const markup = `
     
     `;
-    return `Connected!`;
   }
 }
 
