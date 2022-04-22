3b274b26b705fb0a96fe7d8a69918c96616bf24f
Aleksander Jaworski <aleck156@gmail.com>
2022-04-22 08:23:29 +0200

    create method _generateMarkup

--- src/js/views/paginationView.js
+++ src/js/views/paginationView.js
@@ -3,6 +3,12 @@ import icons from '../../img/icons.svg';
 
 class PaginationView extends View {
   _parentElement = document.querySelector('.pagination');
+
+  _generateMarkup() {
+    const markup = `
+    
+    `;
+  }
 }
 
 export default new PaginationView();
