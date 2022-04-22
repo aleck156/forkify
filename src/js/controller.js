6cf73447f19b3abc2890cb02663f5cae713732b2
Aleksander Jaworski <aleck156@gmail.com>
2022-04-22 09:31:09 +0200

    refactor generating button into _button, apply it in _generateMarkup

--- src/js/controller.js
+++ src/js/controller.js
@@ -40,7 +40,7 @@ const controlSearchResults = async function () {
     await model.loadSearchResults(query);
 
     // 3) render results
-    resultsView.render(model.getSearchResultsPage(1));
+    resultsView.render(model.getSearchResultsPage(3));
 
     // 4) render initial pagination buttons
     paginationView.render(model.state.search);
--- src/js/views/paginationView.js
+++ src/js/views/paginationView.js
@@ -4,6 +4,25 @@ import icons from '../../img/icons.svg';
 class PaginationView extends View {
   _parentElement = document.querySelector('.pagination');
 
+  /**
+   *
+   * @param {'left', 'right'} direction set in which direction the button will point toward
+   * @param {Number} curPage    set the number of current page
+   * @returns
+   */
+  _button(direction, curPage) {
+    return `
+      <button class="btn--inline pagination__btn--${
+        direction === 'right' ? 'next' : 'prev'
+      }">
+        <span>Page ${direction == 'right' ? curPage + 1 : curPage - 1}</span>
+        <svg class="search__icon">
+          <use href="${icons}#icon-arrow-${direction}"></use>
+        </svg>
+      </button>
+    `;
+  }
+
   _generateMarkup() {
     const numPages = Math.ceil(
       this._data.results.length / this._data.resultsPerPage
@@ -14,31 +33,17 @@ class PaginationView extends View {
     console.log(this);
     // page 1, and theere are other pages
     if (curPage === 1 && numPages > 1) {
-      return `
-      <button class="btn--inline pagination__btn--next">
-        <span>Page ${curPage + 1}</span>
-        <svg class="search__icon">
-          <use href="${icons}#icon-arrow-right"></use>
-        </svg>
-      </button>
-      `;
+      return this._button('right', curPage);
     }
 
     // last page, no pages left
     if (curPage === numPages) {
-      return `
-      <button class="btn--inline pagination__btn--prev">
-        <svg class="search__icon">
-          <use href="${icons}#icon-arrow-left"></use>
-        </svg>
-        <span>Page ${curPage - 1}</span>
-      </button>
-    `;
+      return this._button('left', curPage);
     }
 
     // mid page
     if (curPage < numPages) {
-      return `other pages`;
+      return this._button('left', curPage) + this._button('right', curPage);
     }
 
     // page 1, but no other pages
