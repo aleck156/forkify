a5a9774dae2f061b4f07a158fc4150eed906cf07
Aleksander Jaworski <aleck156@gmail.com>
2022-04-25 11:45:46 +0200

    use previewView render to display results

--- src/js/views/resultsView.js
+++ src/js/views/resultsView.js
@@ -1,4 +1,5 @@
 import View from './View';
+import previewView from './previewView';
 import icons from '../../img/icons.svg';
 
 class ResultsView extends View {
@@ -7,27 +8,10 @@ class ResultsView extends View {
   _message = ``;
 
   _generateMarkup() {
-    return this._data.map(this._generateMarkupPreview).join('\n');
-  }
-
-  _generateMarkupPreview(recipe) {
-    const id = window.location.hash.slice(1);
-
-    return `
-      <li class="preview">
-        <a class="preview__link ${
-          recipe.id === id ? 'preview__link--active' : ''
-        }" href="#${recipe.id}">
-          <figure class="preview__fig">
-            <img src="${recipe.image}" alt="${recipe.title}" />
-          </figure>
-          <div class="preview__data">
-            <h4 class="preview__title">${recipe.title}</h4>
-            <p class="preview__publisher">${recipe.publisher}</p>
-          </div>
-        </a>
-      </li>
-      `;
+    console.log(this._data);
+    return this._data
+      .map(result => previewView.render(result, false))
+      .join('\n');
   }
 }
 
