c9fa10375040424816755ea5317f6e35c7a63123
Aleksander Jaworski <aleck156@gmail.com>
2022-04-25 08:28:45 +0200

    change bookmark icon when it is bookmarked

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -89,7 +89,9 @@ class RecipeView extends View {
 
           <button class="btn--round btn--bookmark">
             <svg class="">
-              <use href="${icons}#icon-bookmark"></use>
+              <use href="${icons}#icon-bookmark${
+      this._data.bookmarked ? '-fill' : ''
+    }"></use>
             </svg>
           </button>
         </div>
