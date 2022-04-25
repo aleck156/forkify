4ff5c1ab21ad0f272d86609e996e17f784c44ef1
Aleksander Jaworski <aleck156@gmail.com>
2022-04-25 11:11:25 +0200

    copy code from bookmarksView, delete message properties

--- src/js/views/previewView.js
+++ src/js/views/previewView.js
@@ -1,6 +1,35 @@
 import View from './View';
 import icons from '../../img/icons.svg';
 
-class PreviewView {}
+class PreviewView {
+  _parentElement = document.querySelector('.bookmarks__list');
+
+  _generateMarkup() {
+    console.log(this._data);
+    return this._data.map(this._generateMarkupPreview).join('\n');
+  }
+
+  _generateMarkupPreview(recipe) {
+    const id = window.location.hash.slice(1);
+
+    return `
+        <li class="preview">
+          <a class="preview__link ${
+            recipe.id === id ? 'preview__link--active' : ''
+          }" href="#${recipe.id}">
+            <figure class="preview__fig">
+              <img src="${recipe.image}" alt="${recipe.title}" />
+            </figure>
+            <div class="preview__data">
+              <h4 class="preview__name">
+                ${recipe.title}
+              </h4>
+              <p class="preview__publisher">${recipe.publisher}</p>
+            </div>
+          </a>
+        </li>
+      `;
+  }
+}
 
 export default new PreviewView();
