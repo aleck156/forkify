e7aaeb8563e51c9a3649e58dde9b765667f565f9
Aleksander Jaworski <aleck156@gmail.com>
2022-04-25 10:33:19 +0200

    create bookmarksView class

---
+++ src/js/views/bookmarksView.js
@@ -0,0 +1,34 @@
+import View from './View';
+import icons from '../../img/icons.svg';
+
+class ResultsView extends View {
+  _parentElement = document.querySelector('.results');
+  _errorMessage = `Zero recipes found! Please try again ...`;
+  _message = ``;
+
+  _generateMarkup() {
+    return this._data.map(this._generateMarkupPreview).join('\n');
+  }
+
+  _generateMarkupPreview(recipe) {
+    const id = window.location.hash.slice(1);
+
+    return `
+      <li class="preview">
+        <a class="preview__link ${
+          recipe.id === id ? 'preview__link--active' : ''
+        }" href="#${recipe.id}">
+          <figure class="preview__fig">
+            <img src="${recipe.image}" alt="${recipe.title}" />
+          </figure>
+          <div class="preview__data">
+            <h4 class="preview__title">${recipe.title}</h4>
+            <p class="preview__publisher">${recipe.publisher}</p>
+          </div>
+        </a>
+      </li>
+      `;
+  }
+}
+
+export default new ResultsView();
