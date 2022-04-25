1d46741fc1346e36c3b3d24b9d9b3e45a3c0f195
Aleksander Jaworski <aleck156@gmail.com>
2022-04-25 11:21:49 +0200

    import previewView, comment out preview method, call render from previewView

--- src/js/views/bookmarksView.js
+++ src/js/views/bookmarksView.js
@@ -1,4 +1,5 @@
 import View from './View';
+import previewView from './previewView';
 import icons from '../../img/icons.svg';
 
 class BookmarksView extends View {
@@ -8,30 +9,30 @@ class BookmarksView extends View {
 
   _generateMarkup() {
     console.log(this._data);
-    return this._data.map(this._generateMarkupPreview).join('\n');
+    return this._data.map(bookmark => previewView.render(bookmark)).join('\n');
   }
 
-  _generateMarkupPreview(recipe) {
-    const id = window.location.hash.slice(1);
+  // _generateMarkupPreview(recipe) {
+  //   const id = window.location.hash.slice(1);
 
-    return `
-        <li class="preview">
-          <a class="preview__link ${
-            recipe.id === id ? 'preview__link--active' : ''
-          }" href="#${recipe.id}">
-            <figure class="preview__fig">
-              <img src="${recipe.image}" alt="${recipe.title}" />
-            </figure>
-            <div class="preview__data">
-              <h4 class="preview__name">
-                ${recipe.title}
-              </h4>
-              <p class="preview__publisher">${recipe.publisher}</p>
-            </div>
-          </a>
-        </li>
-      `;
-  }
+  //   return `
+  //       <li class="preview">
+  //         <a class="preview__link ${
+  //           recipe.id === id ? 'preview__link--active' : ''
+  //         }" href="#${recipe.id}">
+  //           <figure class="preview__fig">
+  //             <img src="${recipe.image}" alt="${recipe.title}" />
+  //           </figure>
+  //           <div class="preview__data">
+  //             <h4 class="preview__name">
+  //               ${recipe.title}
+  //             </h4>
+  //             <p class="preview__publisher">${recipe.publisher}</p>
+  //           </div>
+  //         </a>
+  //       </li>
+  //     `;
+  // }
 }
 
 export default new BookmarksView();
