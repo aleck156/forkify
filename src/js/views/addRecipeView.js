9401b14dd07b5af81c237b342f416a18164f05ed
Aleksander Jaworski <aleck156@gmail.com>
2022-04-27 10:44:58 +0200

    copy code from bookmarkView

--- src/js/views/addRecipeView.js
+++ src/js/views/addRecipeView.js
@@ -2,6 +2,20 @@ import View from './View';
 import previewView from './previewView';
 import icons from '../../img/icons.svg';
 
-class AddRecipeView {}
+class AddRecipeView {
+  _parentElement = document.querySelector('.bookmarks__list');
+  _errorMessage = `No bookmarks yet ...`;
+  _message = ``;
+
+  addHandlerRender(handler) {
+    window.addEventListener('load', handler);
+  }
+
+  _generateMarkup() {
+    return this._data
+      .map(bookmark => previewView.render(bookmark, false))
+      .join('\n');
+  }
+}
 
 export default new AddRecipeView();
