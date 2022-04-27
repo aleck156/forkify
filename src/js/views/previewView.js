1a8e4c7af743e326af6bece03280a2d60c88eb90
Aleksander Jaworski <aleck156@gmail.com>
2022-04-27 09:44:42 +0200

    replace h4 class with preview__title

--- src/js/views/previewView.js
+++ src/js/views/previewView.js
@@ -16,7 +16,7 @@ class PreviewView extends View {
               <img src="${this._data.image}" alt="${this._data.title}" />
             </figure>
             <div class="preview__data">
-              <h4 class="preview__name">
+              <h4 class="preview__title">
                 ${this._data.title}
               </h4>
               <p class="preview__publisher">${this._data.publisher}</p>
@@ -28,3 +28,47 @@ class PreviewView extends View {
 }
 
 export default new PreviewView();
+
+// _generateMarkup() {
+//   console.log(this._data);
+
+//   return this._data.map(this._generateMarkupPreview).join('\n');
+// }
+
+// _generateMarkupPreview(recipe) {
+//   const id = window.location.hash.slice(1);
+
+//   return `
+//       <li class="preview">
+//         <a class="preview__link ${
+//           recipe.id === id ? 'preview__link--active' : ''
+//         }" href="#${recipe.id}">
+//           <figure class="preview__fig">
+//             <img src="${recipe.image}" alt="${recipe.title}" />
+//           </figure>
+//           <div class="preview__data">
+//             <h4 class="preview__title">${recipe.title}</h4>
+//             <p class="preview__publisher">${recipe.publisher}</p>
+//           </div>
+//         </a>
+//       </li>
+//       `;
+// }
+
+//        <li class="preview">
+//           <a class="preview__link preview__link--active" href="#23456">
+//             <figure class="preview__fig">
+//               <img src="src/img/test-1.jpg" alt="Test" />
+//             </figure>
+//             <div class="preview__data">
+//               <h4 class="preview__title">Pasta with Tomato Cream ...</h4>
+//               <p class="preview__publisher">The Pioneer Woman</p>
+//               <div class="preview__user-generated">
+//                 <svg>
+//                   <use href="src/img/icons.svg#icon-user"></use>
+//                 </svg>
+//               </div>
+//             </div>
+//           </a>
+//         </li>
+//          -->
