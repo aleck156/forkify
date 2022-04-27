3ac16c5a17822f2a0591d0ceebe37948314234f9
Aleksander Jaworski <aleck156@gmail.com>
2022-04-27 10:52:23 +0200

    grab buttons

--- src/js/views/addRecipeView.js
+++ src/js/views/addRecipeView.js
@@ -4,8 +4,13 @@ import icons from '../../img/icons.svg';
 
 class AddRecipeView extends View {
   _parentElement = document.querySelector('.upload');
+
   _window = document.querySelector('.add-recipe-window');
   _overlay = document.querySelector('.overlay');
+
+  _btnOpen = document.querySelector('.nav__btn--add-recipe');
+  _btnClose = document.querySelector('.btn--close-modal');
+  _btnUpload = document.querySelector('.upload__btn');
 
   addHandlerRender(handler) {
     window.addEventListener('load', handler);
