0cd345ff46a377d3de2481a4b6ec42f145163c0e
Aleksander Jaworski <aleck156@gmail.com>
2022-04-29 09:04:27 +0200

    code indentation fixed

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -84,16 +84,16 @@ class RecipeView extends View {
           </div>
 
           <div class="recipe__user-generated">
-          <svg>
-            <use href="src/img/icons.svg#icon-user"></use>
-          </svg>
+            <svg>
+              <use href="src/img/icons.svg#icon-user"></use>
+            </svg>
+          </div>
+          <button class="btn--round">
+            <svg class="">
+              <use href="src/img/icons.svg#icon-bookmark-fill"></use>
+            </svg>
+          </button>
         </div>
-        <button class="btn--round">
-          <svg class="">
-            <use href="src/img/icons.svg#icon-bookmark-fill"></use>
-          </svg>
-        </button>
-      </div>
 
         <div class="recipe__ingredients">
           <h2 class="heading--2">Recipe ingredients</h2>
