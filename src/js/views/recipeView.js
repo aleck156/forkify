734c3e83a6af80f876e871686786e0ad529fcc34
Aleksander Jaworski <aleck156@gmail.com>
2022-04-29 09:11:52 +0200

    hide user icon when it is not created by that user

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -83,14 +83,17 @@ class RecipeView extends View {
             </div>
           </div>
 
-          <div class="recipe__user-generated">
+          <div class="recipe__user-generated ${this._data.key ? '' : 'hidden'}">
             <svg>
               <use href="${icons}#icon-user"></use>
             </svg>
           </div>
-          <button class="btn--round">
+
+          <button class="btn--round btn--bookmark">
             <svg class="">
-              <use href="src/img/icons.svg#icon-bookmark-fill"></use>
+              <use href="${icons}#icon-bookmark${
+      this._data.bookmarked ? '-fill' : ''
+    }"></use>
             </svg>
           </button>
         </div>
